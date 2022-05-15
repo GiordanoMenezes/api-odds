74e18241070c6e22ebf58350be7ebbc50d5b4edd
Giordano Menezes <giordano.menezes@bol.com.br>
2022-05-15 11:56:16 -0300

    Add Cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
 
 const app = express();
+
+const httpServer = createServer(app);
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 
 app.get('/', (req, res) => res.json({
   message: 'Hello World!',
